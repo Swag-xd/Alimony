758c9af2b99a6a26b526074bd58f13170e2f4b20
Swag-xd <swagatswain2001@gmail.com>
2025-07-15 21:59:41 +0530

    I'm adding Icon

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
+import Icon from "../../assets/CalculatorIcon.jpg"
 import styles from "./header.module.css"
 
-export default class Header extends Component{
-    render(){
-        return(
+export default class Header extends Component {
+    render() {
+        return (
             <>
-            <div className={styles.heading}>
-                <h1 className={styles.headingName}>Alimony Calculator</h1>
-            </div>
+                <div className={styles.heading}>
+
+                    <div className={styles.Icon}>
+                        <img src={Icon} alt="logo" />
+                    </div>
+                    
+                    <div className={styles.headingName}>
+                        <h1 className={styles.Name}>Alimony Calculator</h1>
+                    </div>
+                    
+
+
+                </div>
             </>
         )
     }
